936cbd008c6de83a7964d2db68fcc27dd34e498c
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-22 22:05:58 -0600

    docs: add `jsdoc` comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
+/**
+ * @file Manage the main renderer process, this file is responsible for
+ * rendering the React app in the browser.
+ *
+ * @requires React
+ * @requires ReactDOM
+ */
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT REACT MODULES
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
