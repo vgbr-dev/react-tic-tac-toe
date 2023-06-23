42bdf8a2dc410457a3b27ffb7a6d0b00f4254540
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-22 22:09:19 -0600

    style: add comment

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// » IMPORT APP COMPONENT
+import App from './App';
 
 // ━━ CONSTANTS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
