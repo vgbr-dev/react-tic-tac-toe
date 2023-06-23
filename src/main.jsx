d8606b27c41b60a54861e3d836caf0240fe3a726
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-22 22:07:00 -0600

    style: add semicolons

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@
  */
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT REACT MODULES
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
