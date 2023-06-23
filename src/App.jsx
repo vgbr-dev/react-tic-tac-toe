dd79b8a2852bb7a9620804a70ca3dbae88c6fb3e
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-22 22:10:36 -0600

    docs: add `jsdoc` comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,22 @@
 // » IMPORT REACT MODULES
 import { BrowserRouter as Router } from 'react-router-dom';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// » IMPORT COMPONENTS
+import Routes from './router';
 
-export default App
+// ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `App` component, the main component where the entire `react` application
+ * is managed.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered component.
+ */
+const App = () => (
+  <Router>
+    <Routes />
+  </Router>
+);
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default App;
