6429cd26aeabf22476c5ad7845f7d1ef4b3c595b
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-22 22:23:30 -0600

    docs: add `jsdoc` comments

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/**
+ * @file Contains `vite` configuration file.
+ * @module viteConfig
+ */
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT THIRD PARTIES MODULES
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Vite configuration.
+ *
+ * @typedef {import('vite').UserConfig} UserConfig
+ */
+
+// ━━ MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+/**
+ * Vite configuration.
+ *
+ * @see {@link https://vitejs.dev/config/ UserConfig}
+ * @type {UserConfig}
+ */
+const viteConfig = defineConfig({
   plugins: [react()],
-})
+});
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default viteConfig;
